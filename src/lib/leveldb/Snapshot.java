eb80b3bf3620352f026bd09a1f61024d30e65f6e
root <ben.ravago@aol.com>
2018-08-21 12:47:04 -0400

    rework for jdk10

--- src/lib/leveldb/Snapshot.java
+++
@@ -1,6 +0,0 @@
-package bsd.leveldb;
-
-/**
- * A Snapshot marks a particular fixed state in the database;
- */
-public interface Snapshot {}
--- src/lib/leveldb/io/Cache.java
+++
@@ -1,8 +0,0 @@
-package bsd.leveldb.io;
-
-import java.util.Map;
-import java.util.function.Consumer;
-
-public interface Cache<K,V> extends Map<K,V> {
-    Cache onDelete(Consumer<Entry<K,V>> notify);
-}
